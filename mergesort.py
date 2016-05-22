70224d3f6e20a17a19bcd5d722256e0112171923
Tarun Chunchu <tchunchu@uncc.edu>
2016-05-22 19:48:51 -0400

    Add files via upload
    Python program to implement mergesort taking random array input.

---
+++ mergesort.py
@@ -0,0 +1,42 @@
+__author__ = 'TARUNCH'
+import random
+import time
+
+
+def msort(a):
+    if len(a) > 1:
+        mid = len(a)/2
+        b = a[:mid]
+        c = a[mid:]
+
+
+        msort(b)
+        msort(c)
+        i = 0
+        j = 0
+        k = 0
+        while i < len(b) and j < len(c):
+            if b[i] < c[j]:
+                a[k] = b[i]
+                i = i + 1
+            else:
+                a[k] = c[j]
+                j = j + 1
+            k = k + 1
+
+        while i < len(b):
+            a[k] = b[i]
+            i = i + 1
+            k = k + 1
+        while j < len(c):
+            a[k] = c[j]
+            j = j + 1
+            k = k + 1
+
+st = time.time()
+a = random.sample(range(1,1000),999)
+a.sort(reverse=True)
+msort(a)
+print ("%s sec" ,(time.time()-st)*1000)
+
+
